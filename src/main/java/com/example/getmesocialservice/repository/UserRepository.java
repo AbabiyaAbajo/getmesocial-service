213f7dcecf56c10067e31dd1ee48211fd277596d
Ababiya <abaj0004@algonquinlive.com>
2022-06-14 09:06:47 -0400

    Update uRepo  name variable

--- src/main/java/com/example/getmesocialservice/repository/UserRepository.java
+++ src/main/java/com/example/getmesocialservice/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 public class UserRepository {
 
     public User getUser() {
-        return new User("Ababiya", "123 Mart rd, Heaven, Galaxy", 200, "myPic2022.com");
+        return new User("Ababiya Aba Abajobir", "123 Mart rd, Heaven, Galaxy", 200, "myPic2022.com");
     }
 
 }
